c8ffa0cd19bf2fbee8965b2c4fb98f8798d254ae
Yuriy Husnay <yuriy.husnay@eleks.com>
2018-03-21 16:11:27 +0200

    move everything token related to token service

--- app/services/tokenStorage.js
+++ app/services/tokenStorage.js
@@ -1,13 +1,24 @@
-const Token = require('../models/token');
-
 // Note!
 // Right now using MongoDB for storing refresh tokens
 // TODO: In future it'll be better to migrate using some in-memory DB, eg. Redis
+const mongoose = require('mongoose');
+
+const Token = mongoose.model(
+  'Token',
+  mongoose.Schema(
+    {
+      _id: String,
+      rt: String
+    },
+    {
+      timestamps: true
+    }
+  )
+);
 
 // Note!
 // refresh tokens are stored per user id
 // TODO: rethink
-
 const tokenStorage = {
   getUserToken(userId) {
     return Token.findById(userId)
