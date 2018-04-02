9c552908daabe803f0cf9a344c323056d1ddb11b
Yuriy Husnay <yuriy.husnay@eleks.com>
2018-04-02 14:24:54 +0300

    ACPT-68 Setup testing framework

---
+++ spec/helpers/index.js
@@ -0,0 +1,17 @@
+// Add Jasmine helpers
+
+/* beforeEach(function() {
+  jasmine.addMatchers({
+    toBeCustomMatcher: function() {
+      return {
+        compare: function(actual, expected) {
+          // string equals to expected result
+          // and is equal to 'custom'
+          return {
+            pass: actual === expected && actual === 'custom'
+          };
+        }
+      };
+    }
+  });
+}); */
