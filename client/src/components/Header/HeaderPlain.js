8448b2166f25225a3d7818bca9d41927746edb4f
Yuriy Husnay <yuriy.husnay@eleks.com>
2018-04-02 19:25:22 +0300

    fix signup logo

--- client/src/components/Header/HeaderPlain.js
+++ client/src/components/Header/HeaderPlain.js
@@ -7,7 +7,7 @@ const HeaderPlain = () => {
     <header className="header header__plain">
       <div className="header__logo">
         <span to="/" className="header__logo__link">
-          <img className="header__logo__img" src="/assets/logo.svg" alt="Accept" />
+          <img className="header__logo__img" src="/assets/logo-dark.svg" alt="Accept" />
         </span>
       </div>
     </header>
