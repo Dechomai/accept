caf1a9edf4397eb7ebb9e2ea2f3752725cdaa084
Yuriy Husnay <yuriy.husnay@eleks.com>
2018-04-11 13:16:18 +0300

    fix errors

--- client/src/containers/AboutMe/AboutMe.js
+++ client/src/containers/AboutMe/AboutMe.js
@@ -9,7 +9,7 @@ import AboutMe from '../../components/Profile/AboutMe/AboutMe';
 const mapStateToProps = (state, ownProps) => {
   // if current user
   // check based on route or smth.
-  if (ownProps.userId) {
+  if (!ownProps.userId) {
     return {
       isCurrentUser: true,
       user: selectUserData(state),
--- client/src/containers/UserPanel/UserPanel.js
+++ client/src/containers/UserPanel/UserPanel.js
@@ -6,7 +6,7 @@ import UserPanel from '../../components/UserPanel/UserPanel';
 const mapStateToProps = (state, ownProps) => {
   // if current user
   // check based on route or smth.
-  if (ownProps.userId) {
+  if (!ownProps.userId) {
     return {
       isCurrentUser: true,
       user: selectUserData(state),
