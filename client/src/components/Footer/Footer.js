cd00dc748fedd77d0ca38335fb7cf2f74ea4bdc4
Yuriy Husnay <yuriy.husnay@eleks.com>
2018-04-11 20:11:53 +0300

    ACPT-133 Add basic footer
    update according to new designs

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,13 +1,34 @@
 import './Footer.scss';
 
 import React from 'react';
+import {Link} from 'react-router';
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="container">
-        <img src="/assets/logo.svg" alt="" />
-        <small>Copyright © 2018 AcceptPay Ltd. All Rights Reserved</small>
+        <div className="row pt-4 pb-5">
+          <div className="col-6">
+            <img src="/assets/logo.svg" alt="" />
+          </div>
+          <div className="col-6 text-right">
+            <Link className="footer__link" to="/products">
+              Products
+            </Link>
+            <Link className="footer__link" to="/services">
+              Services
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="footer__copyright">
+        <div className="container">
+          <div className="row py-2">
+            <div className="col-12">
+              <small>Copyright © 2018 AcceptPay Ltd. All Rights Reserved</small>
+            </div>
+          </div>
+        </div>
       </div>
     </footer>
   );
