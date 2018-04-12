1e892cf8bcfd77c63889d47e9340e8700cd93c28
vasyl.hoban <gobankhust@gmail.com>
2018-04-12 15:17:05 +0300

    ACPT-126 added breadcrumbs, added total count

--- client/src/layout/AllProducts.js
+++ client/src/layout/AllProducts.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import AllProductsContainer from '../containers/Products/AllProducts';
 
 const AllProducts = () => {
   return (
-    <div className="all-products__page ">
+    <div className="all-products__page mb-5">
       <div className="container">
         <div className="row">
           <div className="col-12">
-            <h4>AllProducts</h4>
+            <h4 className="mt-5">All Products</h4>
           </div>
         </div>
+        <AllProductsContainer />
       </div>
     </div>
   );
