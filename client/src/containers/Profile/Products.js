d210d72ea7a6725c927436da79871f0da1f712d8
Yuriy Husnay <yuriy.husnay@eleks.com>
2018-04-16 16:27:01 +0300

    fix paging numbers

--- client/src/containers/Profile/Products.js
+++ client/src/containers/Profile/Products.js
@@ -78,7 +78,7 @@ export default compose(
 
         <div className="profile-products__pagination">
           <Pagination
-            totalPages={Math.floor(count / limit)}
+            totalPages={Math.ceil(count / limit)}
             currentPage={Math.floor(skip / limit)}
             onNextClick={onPaginationNextClick}
             onPrevClick={onPaginationPrevClick}
