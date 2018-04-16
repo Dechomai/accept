d0323eeed8a1ca7cc14bf6b50185c859c7cd8d35
Yuriy Husnay <yuriy.husnay@eleks.com>
2018-04-16 12:20:41 +0300

    ACPT-57 View all products
    fix conflicts after merge, add common loader usage

--- client/src/containers/Products/AllProducts.js
+++ client/src/containers/Products/AllProducts.js
@@ -2,18 +2,19 @@ import {compose} from 'ramda';
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router';
 import AllProducts from '../../components/Products/AllProducts';
-import {selectAllProductsFor} from '../../selectors';
+import {selectAllProductsFor, selectAllProductsCount} from '../../selectors';
 import {fetchProducts} from '../../actions/products';
 
 const DEFAULT_LIMIT = 20;
 
 const mapStateToProps = state => ({
-  products: selectAllProductsFor(state, 0, DEFAULT_LIMIT)
+  products: selectAllProductsFor(state, {skip: 0, limit: DEFAULT_LIMIT}),
+  count: selectAllProductsCount(state)
 });
 
 const mapDispatchToProps = dispatch => ({
   fetchProducts() {
-    return dispatch(fetchProducts('all', 0, DEFAULT_LIMIT));
+    return dispatch(fetchProducts({scope: 'all', skip: 0, limit: DEFAULT_LIMIT}));
   }
 });
 
