3e44c9e89a87107bc804f4843ee48ed84f94ee4c
Yuriy Husnay <yuriy.husnay@eleks.com>
2018-04-23 16:43:19 +0300

    ACPT-151 Setup DB migration mechanism

--- app/db/connection.js
+++ app/db/connection.js
@@ -7,14 +7,14 @@ const DB_PASSWORD = config.get('db.password');
 const DB_HOST = config.get('db.host');
 const DB_NAME = config.get('db.name');
 
-const connectionString = () => {
+const getConnectionString = () => {
   let uri = 'mongodb://';
   if (DB_USERNAME && DB_PASSWORD) uri += `${DB_USERNAME}:${DB_PASSWORD}@`;
   return (uri += `${DB_HOST}/${DB_NAME}`);
 };
 
-module.exports = () => {
-  mongoose.connect(connectionString());
+const connect = () => {
+  mongoose.connect(getConnectionString());
   const db = mongoose.connection;
   db.on('error', (...errs) => logger.error('Mongoose connection error: ', ...errs));
   db.once('open', function() {
@@ -24,4 +24,9 @@ module.exports = () => {
       }${DB_HOST}/${DB_NAME}`
     );
   });
+};
+
+module.exports = {
+  connect,
+  getConnectionString
 };
