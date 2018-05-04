d2c54954a4fd6f6970bca4a999b73e957ee5b293
Yuriy Husnay <yuriy.husnay@eleks.com>
2018-05-04 16:33:40 +0300

    ACPT-179 ACPT | Navigation - pages should be displayed from the top
    refactor a bit

--- client/src/layout/App.js
+++ client/src/layout/App.js
@@ -9,18 +9,15 @@ class App extends React.Component {
   constructor() {
     super();
     this.container = null;
-    this.setRef = el => {
-      this.container = el;
-    };
+    this.setRef = el => (this.container = el);
   }
 
   componentDidUpdate(prevProps) {
-    if (this.props.location !== prevProps.location) {
-      if (this.container)
-        this.container.scroll({
-          top: 0,
-          behavior: 'smooth'
-        });
+    if (this.props.location !== prevProps.location && this.container) {
+      this.container.scroll({
+        top: 0,
+        behavior: 'smooth'
+      });
     }
   }
 
