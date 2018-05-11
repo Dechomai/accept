82d371a44017462ee2ce507e676d7d5968d4c0ad
Yuriy Husnay <yuriy.husnay@eleks.com>
2018-05-11 14:37:08 +0300

    MetaMask integration layer

---
+++ client/src/services/metamask.js
@@ -0,0 +1,44 @@
+/* global web3: false */
+
+class MetaMask {
+  isPluginInstalled() {
+    return new Promise((resolve, reject) => {
+      if (typeof web3 !== 'undefined' && web3.currentProvider.isMetaMask) return resolve();
+      reject();
+    });
+  }
+
+  isAcceptNetwork() {
+    return new Promise((resolve, reject) => {
+      this.isPluginInstalled().then(
+        () => {
+          web3.version.getNetwork((err, networkId) => {
+            if (err) return reject(err);
+            if (networkId !== 1011) return reject('Wrong network');
+            resolve();
+          });
+        },
+        () => reject()
+      );
+    });
+  }
+
+  getActiveAccount() {
+    return new Promise((resolve, reject) => {
+      this.isPluginInstalled().then(
+        () => {
+          web3.version.getNetwork((err, accounts) => {
+            if (err) return reject(err);
+            if (accounts.length == 0) return reject('No active account');
+            resolve(accounts[0]);
+          });
+        },
+        () => reject()
+      );
+    });
+  }
+}
+
+const metamaskService = new MetaMask();
+
+export default metamaskService;
