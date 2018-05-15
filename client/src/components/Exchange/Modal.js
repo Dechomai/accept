c49ba0c16b8f498b5ed4726e38f9b9f7dadd5d06
Yuriy Husnay <yuriy.husnay@eleks.com>
2018-05-15 15:53:57 +0300

    add props to control footer, additional btn states

--- client/src/components/Exchange/Modal.js
+++ client/src/components/Exchange/Modal.js
@@ -7,43 +7,55 @@ import {Button, Modal, ModalBody, ModalFooter} from 'reactstrap';
 const ExchangeModal = ({
   title,
   subtitle,
+  showFooter,
+  backBtnDisabled,
+  cancelBtnDisabled,
+  nextBtnDisabled,
+  nextBtnCaption,
   onNextBtnClick,
   onBackBtnClick,
   onCancelBtnClick,
-  nextBtnDisabled,
-  backBtnDisabled,
-  nextBtnCaption,
   children
 }) => (
-  <Modal isOpen={true} toggle={onCancelBtnClick} className="exchange-modal" size="lg">
+  <Modal
+    isOpen={true}
+    toggle={onCancelBtnClick}
+    className="exchange-modal"
+    size="lg"
+    backdrop="static">
     <div className="exchange-modal__header">
       <h5 className="exchange-modal__title">{title}</h5>
       <p className="exchange-modal__subtitle">{subtitle}</p>
     </div>
     <ModalBody>{children}</ModalBody>
-    <ModalFooter>
-      <Button color="link" disabled={backBtnDisabled} onClick={onBackBtnClick}>
-        Back
-      </Button>
-      <div>
-        <Button color="link" onClick={onCancelBtnClick}>
-          Cancel
+    {showFooter && (
+      <ModalFooter>
+        <Button color="link" disabled={backBtnDisabled} onClick={onBackBtnClick}>
+          Back
         </Button>
-        <Button color="primary" disabled={nextBtnDisabled} onClick={onNextBtnClick}>
-          {nextBtnCaption}
-        </Button>
-      </div>
-    </ModalFooter>
+        <div>
+          <Button color="link" disabled={cancelBtnDisabled} onClick={onCancelBtnClick}>
+            Cancel
+          </Button>
+          <Button color="primary" disabled={nextBtnDisabled} onClick={onNextBtnClick}>
+            {nextBtnCaption}
+          </Button>
+        </div>
+      </ModalFooter>
+    )}
   </Modal>
 );
 
 ExchangeModal.propTypes = {
   title: PropTypes.string.isRequired,
   subtitle: PropTypes.string.isRequired,
+  showFooter: PropTypes.bool.isRequired,
+  backBtnDisabled: PropTypes.bool.isRequired,
+  cancelBtnDisabled: PropTypes.bool.isRequired,
+  nextBtnDisabled: PropTypes.bool.isRequired,
   onCancelBtnClick: PropTypes.func.isRequired,
   onNextBtnClick: PropTypes.func.isRequired,
   onBackBtnClick: PropTypes.func.isRequired,
-  nextBtnDisabled: PropTypes.bool.isRequired,
   nextBtnCaption: PropTypes.string.isRequired
 };
 
