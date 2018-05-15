3902b924cd6b231cf9e280f095c24eb61e5ab126
vasyl.hoban <Vasyl.Hoban@eleks.com>
2018-05-15 18:03:36 +0300

    upd

--- client/src/actions/exchange.js
+++ client/src/actions/exchange.js
@@ -1,47 +1,47 @@
-export const SELECT_ITEM_TYPE = 'SELECT_ITEM_TYPE';
-export const SELECT_ITEM = 'SELECT_ITEM';
-export const CHANGE_OWN_COUNT = 'CHANGE_OWN_COUNT';
-export const CHANGE_PARTNER_COUNT = 'CHANGE_PARTNER_COUNT';
-export const CHANGE_OWN_DAYS = 'CHANGE_OWN_DAYS';
-export const CHANGE_OWN_TIME = 'CHANGE_OWN_TIME';
+export const SELECT_EXCHANGE_ITEM_TYPE = 'SELECT_EXCHANGE_ITEM_TYPE';
+export const SELECT_EXCHANGE_ITEM = 'SELECT_EXCHANGE_ITEM';
+export const CHANGE_EXCHANGE_OWN_COUNT = 'CHANGE_EXCHANGE_OWN_COUNT';
+export const CHANGE_EXCHANGE_PARTNER_COUNT = 'CHANGE_EXCHANGE_PARTNER_COUNT';
+export const CHANGE_EXCHANGE_OWN_DAYS = 'CHANGE_EXCHANGE_OWN_DAYS';
+export const CHANGE_EXCHANGE_OWN_TIME = 'CHANGE_EXCHANGE_OWN_TIME';
 
 export const selectItemType = type => ({
-  type: SELECT_ITEM_TYPE,
+  type: SELECT_EXCHANGE_ITEM_TYPE,
   payload: {
     selectedType: type
   }
 });
 
 export const selectItem = item => ({
-  type: SELECT_ITEM,
+  type: SELECT_EXCHANGE_ITEM,
   payload: {
     selectedItem: item
   }
 });
 
 export const changeOwnCount = ownCount => ({
-  type: CHANGE_OWN_COUNT,
+  type: CHANGE_EXCHANGE_OWN_COUNT,
   payload: {
     ownCount
   }
 });
 
 export const changePartnerCount = partnerCount => ({
-  type: CHANGE_PARTNER_COUNT,
+  type: CHANGE_EXCHANGE_PARTNER_COUNT,
   payload: {
     partnerCount
   }
 });
 
 export const changeOwnDays = ownDays => ({
-  type: CHANGE_OWN_DAYS,
+  type: CHANGE_EXCHANGE_OWN_DAYS,
   payload: {
     ownDays
   }
 });
 
 export const changeOwnTime = ownTime => ({
-  type: CHANGE_OWN_TIME,
+  type: CHANGE_EXCHANGE_OWN_TIME,
   payload: {
     ownTime
   }
--- client/src/reducers/exchange.js
+++ client/src/reducers/exchange.js
@@ -1,10 +1,10 @@
 import {
-  SELECT_ITEM_TYPE,
-  SELECT_ITEM,
-  CHANGE_OWN_COUNT,
-  CHANGE_PARTNER_COUNT,
-  CHANGE_OWN_DAYS,
-  CHANGE_OWN_TIME
+  SELECT_EXCHANGE_ITEM_TYPE,
+  SELECT_EXCHANGE_ITEM,
+  CHANGE_EXCHANGE_OWN_COUNT,
+  CHANGE_EXCHANGE_PARTNER_COUNT,
+  CHANGE_EXCHANGE_OWN_DAYS,
+  CHANGE_EXCHANGE_OWN_TIME
 } from '../actions/exchange';
 
 const getInitialState = () => ({
@@ -18,37 +18,37 @@ const getInitialState = () => ({
 
 const exchange = (state = getInitialState(), action) => {
   switch (action.type) {
-    case SELECT_ITEM_TYPE: {
+    case SELECT_EXCHANGE_ITEM_TYPE: {
       return {
         ...state,
         selectedType: action.payload.selectedType
       };
     }
-    case SELECT_ITEM: {
+    case SELECT_EXCHANGE_ITEM: {
       return {
         ...state,
         selectedItem: action.payload.selectedItem
       };
     }
-    case CHANGE_OWN_COUNT: {
+    case CHANGE_EXCHANGE_OWN_COUNT: {
       return {
         ...state,
         ownCount: action.payload.ownCount
       };
     }
-    case CHANGE_PARTNER_COUNT: {
+    case CHANGE_EXCHANGE_PARTNER_COUNT: {
       return {
         ...state,
         partnerCount: action.payload.partnerCount
       };
     }
-    case CHANGE_OWN_DAYS: {
+    case CHANGE_EXCHANGE_OWN_DAYS: {
       return {
         ...state,
         ownDays: action.payload.ownDays
       };
     }
-    case CHANGE_OWN_TIME: {
+    case CHANGE_EXCHANGE_OWN_TIME: {
       return {
         ...state,
         ownTime: action.payload.ownTime
