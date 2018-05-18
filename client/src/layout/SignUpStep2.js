488fd87675a051fc20313d6c20405421b3f350b6
Volodymyr Ilnytskyi <volodymyr.ilnytskyi@eleks.com>
2018-05-18 16:05:22 +0300

    ACPT-292 Change invalid form message

--- client/src/layout/SignUpStep2.js
+++ client/src/layout/SignUpStep2.js
@@ -8,7 +8,7 @@ const SignUpStep2 = () => {
       <div className="container">
         <div className="row text-center">
           <div className="col-12">
-            <h4 className="sign-up__header mt-5">Step2. Create personal profile</h4>
+            <h4 className="sign-up__header mt-5">Step 2. Create personal profile</h4>
             <h6 className="sign-up__subheader mt-4">Please fill in all the required fields</h6>
           </div>
         </div>
