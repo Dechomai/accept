91b05cd1d92a267444dc0b28255874920651df2f
Volodymyr Ilnytskyi <volodymyr.ilnytskyi@eleks.com>
2018-05-18 16:47:15 +0300

    ACPT-295 Hide steps description & image when completed

--- client/src/components/SignUp/Step3.js
+++ client/src/components/SignUp/Step3.js
@@ -79,28 +79,34 @@ class SignUpStep3 extends React.Component {
                 })}
               />
               <h5 className="sign-up-step3__header">Metamask Plugin</h5>
-              <p className="sign-up-step3__description">
-                Looks like you don`t have MetaMask, please install it by the following link{' '}
-                <a href="https://metamask.io/">metamask.io</a> and press check again
-              </p>
-              <Button
-                outline
-                color="primary"
-                disabled={this.state.isPluginInstalled}
-                onClick={this.reloadPage}>
-                Check again
-              </Button>
+              {!this.state.isPluginInstalled && (
+                <React.Fragment>
+                  <p className="sign-up-step3__description">
+                    Looks like you don`t have MetaMask, please install it by the following link{' '}
+                    <a href="https://metamask.io/">metamask.io</a> and press check again
+                  </p>
+                  <Button
+                    outline
+                    color="primary"
+                    disabled={this.state.isPluginInstalled}
+                    onClick={this.reloadPage}>
+                    Check again
+                  </Button>
+                </React.Fragment>
+              )}
             </div>
           </div>
-          <div className="col-4">
-            <div className="sign-up-step3__photo">
-              <img
-                className="sign-up-step3__photo__img"
-                src="/assets/img/metamask-plugin.png"
-                alt="Accept"
-              />
+          {!this.state.isPluginInstalled && (
+            <div className="col-4">
+              <div className="sign-up-step3__photo">
+                <img
+                  className="sign-up-step3__photo__img"
+                  src="/assets/img/metamask-plugin.png"
+                  alt="Accept"
+                />
+              </div>
             </div>
-          </div>
+          )}
         </div>
       </div>
     );
@@ -119,34 +125,40 @@ class SignUpStep3 extends React.Component {
                 })}
               />
               <h5 className="sign-up-step3__header">Account connection</h5>
-              <p className="sign-up-step3__description">
-                Please, create/select MetaMask account which you would like to connect to our Accept
-                Pay Network and press <b>connect account button</b>
-              </p>
-              <Alert color="warning">
-                <Icon name="alert" size="16" />
-                <div>Once you connect Account, you couldn`t change it.</div>
-              </Alert>
-              <div className="form-group">
-                <label>Account address</label>
-                <p className="sign-up-step3__description">
-                  {this.state.activeAccount || 'Error retrieving account information'}
-                </p>
+              {!this.state.activeAccount && (
+                <React.Fragment>
+                  <p className="sign-up-step3__description">
+                    Please, create/select MetaMask account which you would like to connect to our
+                    Accept Pay Network and press <b>connect account button</b>
+                  </p>
+                  <Alert color="warning">
+                    <Icon name="alert" size="16" />
+                    <div>Once you connect Account, you couldn`t change it.</div>
+                  </Alert>
+                  <div className="form-group">
+                    <label>Account address</label>
+                    <p className="sign-up-step3__description">
+                      {this.state.activeAccount || 'Error retrieving account information'}
+                    </p>
+                  </div>
+                  <Button outline color="primary" onClick={this.updateAccountStatus}>
+                    Refresh
+                  </Button>
+                </React.Fragment>
+              )}
+            </div>
+          </div>
+          {!this.state.activeAccount && (
+            <div className="col-4">
+              <div className="sign-up-step3__photo">
+                <img
+                  className="sign-up-step3__photo__img"
+                  src="/assets/img/acc-connection.png"
+                  alt="Accept"
+                />
               </div>
-              <Button outline color="primary" onClick={this.updateAccountStatus}>
-                Refresh
-              </Button>
             </div>
-          </div>
-          <div className="col-4">
-            <div className="sign-up-step3__photo">
-              <img
-                className="sign-up-step3__photo__img"
-                src="/assets/img/acc-connection.png"
-                alt="Accept"
-              />
-            </div>
-          </div>
+          )}
         </div>
       </div>
     );
@@ -165,41 +177,47 @@ class SignUpStep3 extends React.Component {
                 })}
               />
               <h5 className="sign-up-step3__header">Join our Accept Pay Network</h5>
-              <p className="sign-up-step3__description">
-                Please, connect to custom RPC and insert following URL
-              </p>
               {!this.state.isAcceptNetwork && (
-                <Alert color="warning">
-                  <Icon name="alert" size="16" />
-                  <div>
-                    You have to be in Accept Pay Network to proceed with exchanges or other
-                    transactions
+                <React.Fragment>
+                  <p className="sign-up-step3__description">
+                    Please, connect to custom RPC and insert following URL
+                  </p>
+
+                  <Alert color="warning">
+                    <Icon name="alert" size="16" />
+                    <div>
+                      You have to be in Accept Pay Network to proceed with exchanges or other
+                      transactions
+                    </div>
+                  </Alert>
+
+                  <div className="form-group">
+                    <label>Accept Pay Network URL</label>
+                    <div className="sign-up-step3__network-url">
+                      {config.bcNetworkUrl}
+                      <Button color="copy" onClick={this.copyNetworkUrlToClipboard}>
+                        Copy
+                      </Button>
+                    </div>
                   </div>
-                </Alert>
-              )}
-              <div className="form-group">
-                <label>Accept Pay Network URL</label>
-                <div className="sign-up-step3__network-url">
-                  {config.bcNetworkUrl}
-                  <Button color="copy" onClick={this.copyNetworkUrlToClipboard}>
-                    Copy
+                  <Button outline color="primary" onClick={this.updateNetworkStatus}>
+                    Check connection
                   </Button>
-                </div>
+                </React.Fragment>
+              )}
+            </div>
+          </div>
+          {!this.state.isAcceptNetwork && (
+            <div className="col-4">
+              <div className="sign-up-step3__photo">
+                <img
+                  className="sign-up-step3__photo__img"
+                  src="/assets/img/join-accept.png"
+                  alt="Accept"
+                />
               </div>
-              <Button outline color="primary" onClick={this.updateNetworkStatus}>
-                Check connection
-              </Button>
             </div>
-          </div>
-          <div className="col-4">
-            <div className="sign-up-step3__photo">
-              <img
-                className="sign-up-step3__photo__img"
-                src="/assets/img/join-accept.png"
-                alt="Accept"
-              />
-            </div>
-          </div>
+          )}
         </div>
       </div>
     );
