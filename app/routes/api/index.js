3ffba6582e98ab376233bb41903237e3e5c23c55
Yuriy Husnay <yuriy.husnay@eleks.com>
2018-05-21 13:22:59 +0300

    exchanges endpoint

--- app/routes/api/index.js
+++ app/routes/api/index.js
@@ -3,8 +3,9 @@ const express = require('express');
 const userRouter = require('./user');
 const productsRouter = require('./products');
 const servicesRouter = require('./services');
+const exchangesRouter = require('./exchanges');
 
-const routers = [userRouter, productsRouter, servicesRouter];
+const routers = [userRouter, productsRouter, servicesRouter, exchangesRouter];
 
 const PATH = '/api';
 const apiRouter = express.Router();
