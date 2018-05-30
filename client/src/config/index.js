3ea018a08c447453e4e17f69548d9e9ade58e5d2
Yuriy Husnay <yuriy.husnay@eleks.com>
2018-05-30 12:56:22 +0300

    update contract, move to single source

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,3 +1,4 @@
 export default {
-  bcNetworkUrl: 'http://network.accept.io:8545/'
+  bcNetworkUrl: 'http://network.accept.io:8545/',
+  bcTokenContractAddress: '0x8D7991044311Dce2897D923ee010533f44C40560'
 };
