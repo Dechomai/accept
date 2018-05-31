0b00ccce170f061f77cb77316906c8201de57963
Volodymyr Ilnytskyi <volodymyr.ilnytskyi@eleks.com>
2018-05-31 13:38:22 +0300

    ACPT-391 Replace start/finish progress position with paths

--- client/src/components/Exchange/Signature.js
+++ client/src/components/Exchange/Signature.js
@@ -4,10 +4,12 @@ import autobind from 'autobindr';
 
 const SIGNATURE_PATH =
   'm 237.96094,0 c 0,0 -77.52969,77.665619 -116.29492,116.49805 L 47.626953,42.587891 0,90.296875 121.61914,211.84375 238.04492,95.214844 354.67188,211.64062 l 47.5664,-47.65039 z';
+const SIGNATURE_START_PATH =
+  'M 33.177734 123.41602 L 80.845703 75.748047 L 47.626953 42.587891 L 0 90.296875 L 33.177734 123.41602 z ';
+const SIGNATURE_FINISH_PATH =
+  'M 369.23242 131.04102 L 321.62305 178.65039 L 354.67188 211.64062 L 402.23828 163.99023 L 369.23242 131.04102 z';
 const SIGNATURE_IMAGE_URL = '/assets/signature.svg';
 const SIGNATURE_PLACEHOLDER_COLOR = '#ededed';
-const SIGNATURE_INITIAL_PROGRESS = 0.115;
-const SIGNATURE_SUFFICIENT_PROGRESS = 0.885;
 const SIGNATURE_COMPLETION_DELAY = 1000;
 
 const States = {
@@ -22,6 +24,8 @@ class Signature extends React.Component {
     autobind(this);
     this.state = {imageLoaded: false, status: States.INITIAL};
     this.path = new Path2D(SIGNATURE_PATH);
+    this.startPath = new Path2D(SIGNATURE_START_PATH);
+    this.finishPath = new Path2D(SIGNATURE_FINISH_PATH);
     this.image = new Image();
     this.image.src = SIGNATURE_IMAGE_URL;
     this.image.onload = this.onImageLoaded;
@@ -32,7 +36,7 @@ class Signature extends React.Component {
     this.ctx = canvas ? canvas.getContext('2d') : null;
     if (canvas) {
       this.ctx.fillStyle = SIGNATURE_PLACEHOLDER_COLOR;
-      this.drawSignature({progress: SIGNATURE_INITIAL_PROGRESS});
+      this.drawSignature({progress: 0});
       canvas.addEventListener('mousedown', this.handleMouseDown);
       canvas.addEventListener('mousemove', this.handleMouseMove);
       canvas.addEventListener('mouseup', this.handleMouseUp);
@@ -59,25 +63,25 @@ class Signature extends React.Component {
 
   handleMouseDown(e) {
     const {status} = this.state;
-    const {canvas} = this;
+    const {startPath} = this;
     if (status === States.SIGNED) return;
 
-    const pos = this.getMousePos(e);
-    const isSigning = pos.x < canvas.width * SIGNATURE_INITIAL_PROGRESS && this.isMouseInBounds(e);
+    const isSigning = this.isMouseInBounds(e, startPath);
     this.setState({status: isSigning ? States.SIGNING : States.INITIAL});
   }
 
   handleMouseMove(e) {
-    const {canvas, image} = this;
+    const {canvas, path, finishPath} = this;
     const {status} = this.state;
 
     if (!canvas || status === States.SIGNED) return false;
 
     const pos = this.getMousePos(e);
     if (status === States.SIGNING) {
-      const isInBounds = this.isMouseInBounds(e);
+      const isInBounds = this.isMouseInBounds(e, path);
+      const isInFinishZone = this.isMouseInBounds(e, finishPath);
       if (isInBounds) {
-        if (pos.x >= image.width * SIGNATURE_SUFFICIENT_PROGRESS) {
+        if (isInFinishZone) {
           this.drawSignature({progress: 1});
           this.setState({status: States.SIGNED});
           setTimeout(this.props.onSigned, SIGNATURE_COMPLETION_DELAY);
@@ -85,7 +89,7 @@ class Signature extends React.Component {
           this.drawSignature({width: pos.x});
         }
       } else {
-        this.drawSignature({progress: SIGNATURE_INITIAL_PROGRESS});
+        this.drawSignature({progress: 0});
         this.setState({status: States.INITIAL});
       }
       return isInBounds;
@@ -97,7 +101,7 @@ class Signature extends React.Component {
   handleMouseUp() {
     if (this.state.status === States.SIGNING) {
       this.setState({status: States.INITIAL});
-      this.drawSignature({progress: SIGNATURE_INITIAL_PROGRESS});
+      this.drawSignature({progress: 0});
     }
   }
 
@@ -124,8 +128,8 @@ class Signature extends React.Component {
     };
   }
 
-  isMouseInBounds(e) {
-    const {ctx, path} = this;
+  isMouseInBounds(e, path) {
+    const {ctx} = this;
     const pos = this.getMousePos(e);
 
     return ctx.isPointInPath(path, pos.x, pos.y);
