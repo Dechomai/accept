aa8246336149f8ec9444ad1e6c8f180fc60c38aa
Yuriy Husnay <yuriy.husnay@eleks.com>
2018-06-07 13:00:13 +0300

    ACPT-436 "Offer to Exchange: Result" popup - change text

--- client/src/components/Exchange/Confirmation.js
+++ client/src/components/Exchange/Confirmation.js
@@ -6,7 +6,7 @@ import PropTypes from 'prop-types';
 const TITLES = {
   waiting: 'Waiting for transaction acceptance',
   rejected: 'Transaction Rejected',
-  accepted: 'Transaction Complete'
+  accepted: 'Transaction Sent'
 };
 
 const DETAILS = {
