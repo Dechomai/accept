ca1ecf47ab0766e4f2084bb4cf6e1bc3ffa05d1a
vasyl.hoban <Vasyl.Hoban@eleks.com>
2018-06-14 15:46:41 +0300

    ACPT-473 upd

--- client/src/components/SignUp/Step4.js
+++ client/src/components/SignUp/Step4.js
@@ -2,7 +2,13 @@ import './Step4.scss';
 
 import React from 'react';
 import {Button} from 'reactstrap';
+import config from '../../config';
+import clipboard from '../../services/clipboard';
 import PropTypes from 'prop-types';
+
+const copyTokenContractAddressToClipboard = () => {
+  clipboard.write(config.bcTokenContractAddress);
+};
 
 const SignUpStep4 = ({onClickOk}) => {
   return (
@@ -26,6 +32,15 @@ const SignUpStep4 = ({onClickOk}) => {
                     In order to display your current Fulcrum balance in MetaMask, please use “Add
                     Token” option within MetaMask plugin by using the following token address:
                   </p>
+                  <div className="form-group">
+                    <label>Token contract Address</label>
+                    <div className="sign-up-step4__contract-address__url">
+                      {config.bcTokenContractAddress}
+                      <Button color="copy" onClick={copyTokenContractAddressToClipboard}>
+                        Copy
+                      </Button>
+                    </div>
+                  </div>
                   <div className="sign-up-step4__info-images">
                     <img
                       className="sign-up-step4__info-images__img"
