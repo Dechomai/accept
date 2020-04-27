704a83ca5ede6c26ab4532bbe8573eaf57450b82
Yuriy Husnay <yuriy.husnay@eleks.com>
2020-04-27 13:02:44 +0300

    comment out sending bonus ether

--- app/controllers/api/user.js
+++ app/controllers/api/user.js
@@ -117,7 +117,7 @@ const userController = {
 
   sendBonusTokens(address) {
     // try to send bonus ether
-    blockchainService.sendBonusEther(address);
+    // blockchainService.sendBonusEther(address);
     // send bonus tokens
     return blockchainService.sendUserBonus(address);
   }
